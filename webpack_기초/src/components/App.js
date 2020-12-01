c05d0c375740f39cf815730ec34c2ad945a49ba5
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-01 17:04:28 +0900

    webpack 설정

--- webpack_기초/src/components/App.js
+++ webpack_기초/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ImgTest from "../assets/test.png";
 
 const App = () => {
   const [number, setNumber] = useState(0);
@@ -12,6 +13,7 @@ const App = () => {
       <h1>Hello, Webpack~!!!</h1>
       <h3>현재 숫자 : {number}</h3>
       <button onClick={onClickPlus}>PLUS</button>
+      <img src={ImgTest} alt="IMGTEST" />
     </>
   );
 };
--- webpack_기초/webpack.config.js
+++ webpack_기초/webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
           presets: ["@babel/preset-env", "@babel/preset-react"],
         },
       },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "file-loader",
+      },
     ],
   },
   plugins: [
