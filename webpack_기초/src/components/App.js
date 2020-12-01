5bb0eeef2a352c36002c28540c037050c8c9b165
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-01 14:50:24 +0900

    gh-pages, clean-webpack

--- webpack_기초/src/components/App.js
+++ webpack_기초/src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <div>Hello, Webpack!</div>
+      <div>Hello, Webpack~!!!</div>
     </>
   );
 };
--- webpack_기초/webpack.config.js
+++ webpack_기초/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   name: "webpack_test",
@@ -31,5 +32,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "public/index.html",
     }),
+    new CleanWebpackPlugin(),
   ],
 };
